04a7b8bfc2b09daba549b9e96e59c2bf8d9a5b2f
AgnieszkaUcinska <44909472+AgnieszkaUcinska@users.noreply.github.com>
2019-05-14 18:39:50 +0200

    fixed modulo bug

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz():
     for i in range (1,101):
-        if i % 3:
+        if not i % 3:
             print ('Fizz')
-        elif i % 5:
+        elif not i % 5:
             print ('Buzz')
         else:
             print (i)
