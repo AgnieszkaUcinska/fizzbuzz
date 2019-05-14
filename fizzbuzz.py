fe00fbffa95e7109537daff243fe05bb98bb3a5f
AgnieszkaUcinska <44909472+AgnieszkaUcinska@users.noreply.github.com>
2019-05-14 18:46:13 +0200

    Implementef Fizzbuzz part

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz():
     for i in range (1,101):
+        if not i % 3 and not i % 5:
+            print ('Fizzbuzz')
         if not i % 3:
             print ('Fizz')
         elif not i % 5:
